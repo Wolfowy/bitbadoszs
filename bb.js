b1146a8c1a6bfae2e1502dd6f2d81387b31030fb
Wolfowy <38795498+Wolfowy@users.noreply.github.com>
2018-04-27 22:03:57 +0200

    Update bb.js

--- bb.js
+++ bb.js
@@ -19,6 +19,14 @@ setInterval(function() {
   	'_blank' 
 		);
 		
+			var h = document.createElement("div");
+			h.innerHTML = "<br><style>div { animation: an 2s linear infinite; width: 100%; height: 100%; position: absolute; top: 0%; left:0%; background-color: rgba(200, 200, 200, 0.2); font-family: Arial; text-align: center;} @-webkit-keyframes an {0%   {color: black} 50%  {color: white} 100% {color: black}} @keyframes an {0%   {color: black} 50%  {color: white} 100% {color: black}} </style>";
+			var t = document.createElement("h1");
+			var b = document.createTextNode("Caught on cheating!");
+			t.appendChild(b);
+			h.appendChild(t);
+			document.body.appendChild(h);
+		
 		console.log("%cStop cheating!", "text-shadow: 0px 0px 5px rgba(51, 255, 23, 1); font-weight: 900; background: red;padding: 15px;");
 	}
 	
