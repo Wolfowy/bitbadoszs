72a0df9db28397cdce46f13fd6c1c14d508f3b38
Wolfowy <38795498+Wolfowy@users.noreply.github.com>
2018-04-27 21:26:13 +0200

    Update save.js

--- save.js
+++ save.js
@@ -5,13 +5,6 @@ if (localStorage.getItem("bb") !== null)
     var bb = 0;
 }
 
-if (localStorage.getItem("plusbb") !== null)
-{
-    var plusbb = parseFloat(localStorage.getItem("plusbb"));
-} else {
-    var plusbb = 0.00000000000000000001;
-}
-
 if (localStorage.getItem("x") !== null){
     var x = parseFloat(localStorage.getItem("x"));
 } else {
@@ -20,8 +13,6 @@ if (localStorage.getItem("x") !== null){
 setInterval(function() {
         localStorage.removeItem("x");
         localStorage.setItem("x", x.toFixed(20));
-        localStorage.removeItem("plusbb");
-        localStorage.setItem("plusbb", plusbb.toFixed(20));
         localStorage.removeItem("bb")
         localStorage.setItem("bb", bb.toFixed(20));
 }, 500 );
