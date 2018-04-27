102da64615a13a10d8667ce2d5f81699c79b67ec
Wolfowy <38795498+Wolfowy@users.noreply.github.com>
2018-04-27 21:27:38 +0200

    Update bb.js

--- bb.js
+++ bb.js
@@ -1,5 +1,13 @@
 setInterval(function() {
 
+	
+	if (localStorage.getItem("plusbb") !== null)
+	{
+	    var plusbb = parseFloat(localStorage.getItem("plusbb"));
+	} else {
+ 	   var plusbb = 0.00000000000000000001;
+	}
+	
 	bb = bb + plusbb;
 	x = x + plusbb;
 	
@@ -13,5 +21,8 @@ setInterval(function() {
 		
 		console.log("%cStop cheating!", "text-shadow: 0px 0px 5px rgba(51, 255, 23, 1); font-weight: 900; background: red;padding: 15px;");
 	}
+	
+	localStorage.removeItem("plusbb");
+        localStorage.setItem("plusbb", plusbb.toFixed(20));
 
 }, 100 );
