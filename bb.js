f30bd69da333fe2c351cad8bb751df3b87240c08
Wolfowy <38795498+Wolfowy@users.noreply.github.com>
2018-04-27 21:25:09 +0200

    Update bb.js

--- bb.js
+++ bb.js
@@ -4,5 +4,14 @@ setInterval(function() {
 	x = x + plusbb;
 	
 	document.getElementById("miner").innerHTML = bb.toFixed(20) + " BitBadosz's"
+	
+	if(localStorage.getItem("ch") == 1){
+		window.open(
+  	'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  	'_blank' 
+		);
+		
+		console.log("%cStop cheating!", "text-shadow: 0px 0px 5px rgba(51, 255, 23, 1); font-weight: 900; background: red;padding: 15px;");
+	}
 
 }, 100 );
